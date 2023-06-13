87cc56a2c201c437d71d570e401e93bcc496a646
osber <osberriojalopez@gmail.com>
2023-06-13 10:17:20 -0400

    deberia devolver un vector contando al cantidad de ocurrencias de cada palabra

--- src/contador.js
+++ src/contador.js
@@ -1,4 +1,4 @@
-function ContarPalabras(texto,palabra) {
+export function ContarPalabras(texto,palabra) {
   let contador=0;
   if(texto==""){
     return contador;
@@ -15,4 +15,19 @@ function ContarPalabras(texto,palabra) {
   }
 }
 
-export default ContarPalabras;
+export function ContarOcurrencias(texto)
+{
+  let words=texto.split(" ");
+  let cuenta=[];
+  let repeticiones;
+  var i=0;
+  while(i<words.length)
+  {
+    repeticiones=ContarPalabras(texto,words[i]);
+    cuenta.push(repeticiones);
+    i++;
+  }
+  return cuenta;
+}
+
+// export default ContarPalabras;
--- src/contador.test.js
+++ src/contador.test.js
@@ -1,4 +1,4 @@
-import ContarPalabras from "./contador.js";
+import {ContarPalabras,ContarOcurrencias} from "./contador.js";
 
 describe("Dada una frase contar la cantidad de ocurrencias de las palabras", () => {
   it("deberia devolver 0 si el texto esta vacio", () => {
@@ -9,5 +9,9 @@ describe("Dada una frase contar la cantidad de ocurrencias de las palabras", () 
   });
   it("deberia devolver la cantidad de veces que se repite una palabra eb un texto", () => {
     expect(ContarPalabras("hola a todos hola a ti","hola")).toEqual(2);
+  });
+  it("devuelve un vector con con la ocurrencia de 1 en cada palabra", () => {
+    let texto="hola mundo adios mundo hola";
+    expect(ContarOcurrencias(texto)).toEqual([2,2,1,2,2]);
   });
 });
